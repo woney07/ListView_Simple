bad727de1643e2a84907494a6d6b96c9b9859d58
JONG WON LEE <woney07@nate.com>
2017-08-06 23:42:52 +0900

    초기 커밋.

---
+++ ListView_Simple/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ListView_Simple
+{
+    [Activity(Label = "ListView_Simple", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
